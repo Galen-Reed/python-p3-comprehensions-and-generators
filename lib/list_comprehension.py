b15fb3df154ef7cb690a339987123735dab5d068
GalenReed <galenreed@vt.edu>
2024-12-13 17:38:25 -0500

    lists!

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_num_list = [ n for n in num_list if n% 2 == 0]
+
+    if not even_num_list:
+        return []
+    
+    return even_num_list
+
 
 def make_exclamation(sentence_list):
-    pass
+    bold_exclamation = [n + '!' for n in sentence_list]
+
+    if not sentence_list:
+        return []
+    
+    return bold_exclamation
